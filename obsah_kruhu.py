53a1ab4e86cbf656829345da84b8745f0b2ade11
vaclavsv1 <pollsrebsky12@gmail.com>
2024-01-23 09:27:26 +0100

    1. hodina

---
+++ obsah_kruhu.py
@@ -0,0 +1,11 @@
+#Přivítaní uživtele
+print("Vítejte v aplikaci výpočet obsah kruhu")
+      
+r = input("zadejte proměnou r")
+π = 3.14
+
+if r>0:
+    vysledek = π*r²
+    print("Výsledek je“,vysledek")
+else:
+   print("Tak jsi nadrbaný")
---
+++ obsah_obdelniku.py
@@ -0,0 +1,14 @@
+print(“vitejte v aplikaci pro vypocet obvodu obdelniku“)
+
+a = input(“Zadejte proměnou a: “)
+b = input(“Zadejte proměnou b: “)
+
+a = int(a)
+b = int(b)
+
+if a>0 and b>0
+   vysledek = a*b 
+   print(“vysledek je“, výsledek)
+
+else:
+    print(“zkus to znovu“)
